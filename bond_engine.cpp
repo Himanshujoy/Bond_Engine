132f15f4265bfbafeb1aab159e772f1cdf14af1a
Himanshujoy <himanshu.srivastava.cd.che20@itbhu.ac.in>
2026-01-13 17:53:22 +0530

    Adding Bond and basic Cashflow

--- bond_engine.cpp
+++ bond_engine.cpp
@@ -10,6 +10,7 @@ class Date
 {
     private:
         int year_d, month_d, day_d;
+
     public:
         Date(int year, int month, int day) : year_d(year), month_d(month), day_d(day) {}
         
@@ -35,6 +36,7 @@ class FlatYieldCurve : public IYieldCurve
 {
     private:
         double rate_d;
+
     public:
         explicit FlatYieldCurve(double rate) : rate_d(rate) {}
         
@@ -55,6 +57,50 @@ struct Cashflow
     double time;
     double amount;
     string type;
+};
+
+class Bond
+{
+    private:
+        double facevalue_d, couponrate_d;
+        int numyears_d, frequency_d, settlementdays_d;
+
+    public:
+        Bond(double facevalue, double couponrate, int numyears, int frequency, int settlementdays=1)
+            : facevalue_d(facevalue), couponrate_d(couponrate), numyears_d(numyears), frequency_d(frequency), settlementdays_d(settlementdays) {}
+
+        vector<Cashflow> generateCashflows(const Date& settledate) const
+        {
+            vector<Cashflow> cashflows;
+            double couponamount = facevalue_d * couponrate_d / frequency_d;
+            
+            const int days_in_year = 365;
+            const int coupon_interval_days = days_in_year / frequency_d;
+            
+            //Adding coupon cashflows
+            for(int i=1; i <= numyears_d*frequency_d; i++)
+            {
+                Date paymentdate(
+                    settledate.year_d + i / frequency_d,
+                    (settledate.month_d + (i % frequency_d) * 12 / frequency_d)%12 + 1,
+                    settledate.day_d
+                );
+
+                double time = i*1.0/frequency_d;
+                cashflows.push_back({paymentdate, time, couponamount, "Coupon"});
+            }
+
+            //Adding principal repayment cashflow
+            Date maturitydate(
+                settledate.year_d + numyears_d,
+                settledate.month_d,
+                settledate.day_d
+            );
+
+            cashflows.push_back({maturitydate, static_cast<double>(numyears_d), facevalue_d+couponamount, "Principal"});
+
+            return cashflows;
+        }
 };
 
 void main()
